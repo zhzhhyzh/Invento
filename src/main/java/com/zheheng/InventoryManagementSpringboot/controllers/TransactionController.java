6444bdf2b879365a688b2568848baf1a9abd9417
Zheheng <henryyzh0309@gmail.com>
2025-10-26 23:34:58 +0800

    Transaction Controller (Routing)

---
+++ src/main/java/com/zheheng/InventoryManagementSpringboot/controllers/TransactionController.java
@@ -0,0 +1,64 @@
+package com.zheheng.InventoryManagementSpringboot.controllers;
+
+import com.zheheng.InventoryManagementSpringboot.dtos.Response;
+import com.zheheng.InventoryManagementSpringboot.dtos.TransactionRequest;
+import com.zheheng.InventoryManagementSpringboot.enums.TransactionStatus;
+import com.zheheng.InventoryManagementSpringboot.services.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/purchase")
+    public ResponseEntity<Response> purchase(@RequestBody @Valid TransactionRequest transactionRequest) {
+        return ResponseEntity.ok(transactionService.purchase(transactionRequest));
+    }
+
+    @PostMapping("/sell")
+    public ResponseEntity<Response> sell(@RequestBody @Valid TransactionRequest transactionRequest) {
+        return ResponseEntity.ok(transactionService.sell(transactionRequest));
+    }
+
+    @PostMapping("/return")
+    public ResponseEntity<Response> returnToSupplier(@RequestBody @Valid TransactionRequest transactionRequest) {
+        return ResponseEntity.ok(transactionService.returnToSupplier(transactionRequest));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllTransactions(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "1000") int size,
+            @RequestParam(required = false) String searchValue
+    ) {
+        return ResponseEntity.ok(transactionService.getAllTransactions(page, size, searchValue));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getAllTransactionById(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(transactionService.getAllTransactionById(id));
+    }
+
+    @GetMapping("/bydate")
+    public ResponseEntity<Response> getAllTransactionByMonthAndYear(
+            @RequestParam int month, @RequestParam int year
+    ) {
+        return ResponseEntity.ok(transactionService.getAllTransactionByMonthAndYear(month, year));
+    }
+
+    @GetMapping("/update-status")
+    public ResponseEntity<Response> updateTransactionStatus(
+            @PathVariable Long transactionId,
+            @RequestBody TransactionStatus transactionStatus
+    ) {
+        return ResponseEntity.ok(transactionService.updateTransactionStatus(transactionId, transactionStatus));
+    }
+}
